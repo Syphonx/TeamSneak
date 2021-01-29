44dbe521e2828d4f67bbc3e1bb87b6a9173803c1
Jak <contact@jakbarnes.co.uk>
2021-01-29 22:24:38 +0000

    Added data assets and start of dialogue system

---
+++ Source/TeamSneak/TeamSneak.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "TeamSneak.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, TeamSneak, "TeamSneak" );
